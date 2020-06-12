4c5281bb256217f9ab50b89b9089e4c6a6788491
Gabe House <g@Gabes-MacBook-Pro.local>
2020-06-11 23:31:54 -0400

    Updates

---
+++ precache-manifest.bc11b6cc4f6bf0f4a0bbed48269b4b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa98e3c6c6b03a48b8b679dd8316e5a",
+    "url": "/align/index.html"
+  },
+  {
+    "revision": "70ad1050b7f746693bf6",
+    "url": "/align/static/css/main.42f729a7.chunk.css"
+  },
+  {
+    "revision": "a7fca45342f5de80f401",
+    "url": "/align/static/js/2.173d0a31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/align/static/js/2.173d0a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ad1050b7f746693bf6",
+    "url": "/align/static/js/main.6e41828c.chunk.js"
+  },
+  {
+    "revision": "02758285ab85dd58f16c",
+    "url": "/align/static/js/runtime-main.020cda0a.js"
+  }
+]);
